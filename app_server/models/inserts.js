c4ef73e0923a8c0dbf87522b07e38be04fab76d3
EliasSimoes <eliasunipdesign@gmail.com>
2016-11-16 00:24:47 -0200

    Schema com mongoose finalizado

---
+++ app_server/models/inserts.js
@@ -0,0 +1,8 @@
+db.locations.save({
+    name:'Starcups',
+            address:'125 High Street, Reading, RG6 1PS',
+            rating: 3,
+            facilities:['Hot drinks', 'Food', 'Premium wifi'],
+            coords: {
+                lat:51.455041, lng: -0.9690884
+})
--- app_server/models/locations.js
+++ app_server/models/locations.js
@@ -3,25 +3,27 @@ var mongoose = require('mongoose');
 
 var reviewSchema = new mongoose.Schema({
     author: String,
-    rating: {type: Number, require: true, min: 0, max: 5},
+    rating: { type: Number, require: true, min: 0, max: 5 },
     reviewText: String,
-    createdOn: {type: Date, "default": Date.now}
+    createdOn: { type: Date, "default": Date.now }
 
 });
 
 var openingTimeSchema = new mongoose.Schema({
-    days: {type: String, require: true},
+    days: { type: String, require: true },
     opening: String,
     closing: String,
-    closed: {type: Boolean, require: true}
+    closed: { type: Boolean, require: true }
 });
 
 var locationSchema = new mongoose.Schema({
-    name: {type: String, require: true},
+    name: { type: String, require: true },
     address: String,
-    rating: {type: Number, "default": 0, min: 0, max: 5},
+    rating: { type: Number, "default": 0, min: 0, max: 5 },
     facilities: [String],
-    coords: {type: [Number], index: '2dsphare'},
+    coords: { type: [Number], index: '2dsphare' },
     openingTimes: [openingTimeSchema],
     reviews: [reviewSchema]
 });
+
+mongoose.model('Location', locationSchema); //parametro 1 é o nome do modelo, o parametro 2 é o nome do Schema
